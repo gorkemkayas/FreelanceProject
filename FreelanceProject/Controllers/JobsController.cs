1710a503afea3bcb067d7731b93f583d29025ebb
Büşra Basan <124467787+Busrabasan67@users.noreply.github.com>
2025-04-25 16:46:57 +0300

    Jobs ve Applications controllerinin viewları tasarlandı.

---
+++ FreelanceProject/Controllers/JobsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreelanceProject.Controllers
+{
+    public class JobsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details() // iş detaylarını görüntüle.
+        {
+            return View();
+
+        }
+        public IActionResult Create() // Yeni iş ilanı  oluşturma
+        {
+            return View();
+        }
+        public IActionResult Edit()  // İş ilanını düzenleme
+        {
+             return View();
+        }
+        public IActionResult Delete()  // İş ilanını  silme
+        {
+            return View();
+        }
+    }
+}
