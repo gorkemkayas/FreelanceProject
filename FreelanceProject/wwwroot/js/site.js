ffd92bc05dd5db7a5ce66220faf3e513670efd37
Görkem Kaya <gorkemkayas@hotmail.com>
2025-04-29 13:51:34 +0300

    admin paneli eklendi

--- FreelanceProject/wwwroot/js/site.js
+++ FreelanceProject/wwwroot/js/site.js
@@ -2,3 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Initialization for ES Users
+import { Dropdown, initMDB } from "mdb-ui-kit";
+
+initMDB({ Dropdown });
+
+const triggers = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+];
+const basicInstances = [
+    'alert-primary',
+    'alert-secondary',
+    'alert-success',
+    'alert-danger',
+    'alert-warning',
+    'alert-info',
+    'alert-light',
+    'alert-dark',
+];
+
+triggers.forEach((trigger, index) => {
+    let basicInstance = mdb.Alert.getInstance(document.getElementById(basicInstances[index]));
+    document.getElementById(trigger).addEventListener('click', () => {
+        basicInstance.show();
+    });
+});
